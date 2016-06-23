5b75d989937f72a9b1260ec8b5304d912133defc
Cade Rea <cade@caderea.com>
2016-06-23 00:56:14 -0500

    Moved event handling to MouseBot class, removed event listener class. Defined command chain for event lifecycle. Refactored CommandEvent.

--- src/main/java/MouseBot.java
+++ src/main/java/MouseBot.java
@@ -17,16 +17,14 @@ import sx.blah.discord.util.MissingPermissionsException;
 public class MouseBot {
 
     private static String BOT_TOKEN = "MTk0ODUyNDQxMTMxMzE5Mjk3.CkusdA.kawvMg6ptxG92c4Fnvq4651vWgA";
-
+    private final static String KEY = "~";
     public static MouseBot mouseBot;
-    public IDiscordClient discordClient;
 
+    private IDiscordClient discordClient;
     private IChannel channel;
-    private CommandListener commandListener;
 
     public static void main (String[] args) {
         mouseBot = new MouseBot();
-
     }
 
     public MouseBot() {
@@ -39,74 +37,82 @@ public class MouseBot {
 
         this.channel = discordClient.getChannelByID("\\#00");
 
-        //register the CommandListener once the discord client is ready
+        //register mouseBot as a listener once the discord client is ready
         discordClient.getDispatcher().registerListener(new IListener<ReadyEvent>() {
             public void handle(ReadyEvent event) {
-                discordClient.getDispatcher().registerListener(new CommandListener());
+                sendToChannel("mouse-bot ready");
+                discordClient.getDispatcher().registerListener(mouseBot);
             }
         });
     }
 
-    public void sayHello() throws MissingPermissionsException, HTTP429Exception, DiscordException {
-        channel.sendMessage("Helloo!");
+    /**
+     * Submits the message to the channel.
+     * This is the last stop for processing messages.
+     *
+     * @param message
+     */
+    private void sendToChannel(String message) {
+        if (channel != null){
+            try {
+                channel.sendMessage(message);
+            } catch (MissingPermissionsException | HTTP429Exception | DiscordException e) {
+                log.warning("sendToChannel() error: " + e.getMessage());
+            }
+        } else {
+            log.info(message);
+        }
+
     }
 
-    private class CommandListener {
-        final static String KEY = "~";
+    /**
+     * Monitors all messages for commands.
+     * This is the first stop for processing all messages in the channel
+     *
+     * @param event
+     */
+    @EventSubscriber
+    public void watchForCommands (MessageReceivedEvent event) {
+        IMessage message = event.getMessage();
+        String content = message.getContent();
 
-        public CommandListener() {
+        if (!content.startsWith(KEY))
+            return;
 
-        }
-
-        /**
-         * This is the first stop for processing all messages in the channel
-         *
-         * @param event
-         */
-        @EventSubscriber
-        public void watchForCommands (MessageReceivedEvent event) {
-            IMessage message = event.getMessage();
-            String content = message.getContent();
-
-            if(!content.startsWith(KEY))
-                return;
-
-            String command = content.toLowerCase();
-            String[] args = null;
-
-            if(content.contains(" ")) {
-                command = command.split(" ")[0];
-                args = content.substring(content.indexOf(' ') + 1).split(" ");
-            }
-
-            //dispatch a new event for this class to handle
-            discordClient.getDispatcher().dispatch(new CommandExecutionEvent(message, command, message.getAuthor(), args));
-        }
-
-        @EventSubscriber
-        public void ping(CommandExecutionEvent event) {
-            if (event.isCommand("ping")) {
-                try {
-                    event.getMessage().reply("pong");
-                } catch (MissingPermissionsException | HTTP429Exception | DiscordException e) {
-
-                }
-            }
-
-        }
+        dispatchCommandEvent(message, content);
     }
 
-    private class  CommandExecutionEvent extends Event {
+    private void dispatchCommandEvent(IMessage message, String content) {
+        //parse arguments from command
+        String command = content.toLowerCase();
+        String[] args = null;
+
+        if(content.contains(" ")) {
+            command = command.split(" ")[0];
+            args = content.substring(content.indexOf(' ') + 1).split(" ");
+        }
+
+        //dispatch a new event for this class to handle
+        discordClient.getDispatcher().dispatch(new CommandEvent(message, command, args));
+    }
+
+    @EventSubscriber
+    public void ping(CommandEvent event) {
+        if (event.isCommand("ping"))
+            sendToChannel("pong");
+    }
+
+    private class CommandEvent extends Event {
         private final IMessage message;
         private final String command;
         private final IUser author;
         private final String[] args;
 
-        public CommandExecutionEvent(IMessage message, String command, IUser author, String[] args)
+        public CommandEvent(IMessage message, String command, String[] args)
         {
             this.message = message;
             this.command = command;
-            this.author = author;
+            this.author = message.getAuthor();
             this.args = args;
         }
 
@@ -122,7 +128,7 @@ public class MouseBot {
 
         public boolean isCommand(String command)
         {
-            return command.equalsIgnoreCase(command);
+            return this.command.equalsIgnoreCase(command);
         }
 
         public IUser getAuthor()
@@ -130,31 +136,4 @@ public class MouseBot {
             return author;
         }
     }
-
-
 }
-
-/* Event Examples
-
-
-    private static class EventHandler {
-        @EventSubscriber
-        public void onReadyEvent(ReadyEvent event) {
-            System.out.println("The bot is now ready");
-            doSomething();
-        }
-
-        @EventSubscriber
-        public void onMessageEvent(MessageReceivedEvent event) {
-            System.out.println(event.getMessage().getAuthor().getName() + ": " + event.getMessage().getContent());
-        }
-    }
-
-    private static class ReadyEventListener implements IListener<ReadyEvent> {
-        public void handle(ReadyEvent event) {
-            System.out.println("bot ready");
-            doSomething();
-        }
-    }
-
-     */
